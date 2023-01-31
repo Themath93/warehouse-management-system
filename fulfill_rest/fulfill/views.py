8d262e55fc9b68b0bc6e84acfd2b50c79eaf4d8f
Yoon <lms4678@naver.com>
2023-01-31 20:13:15 +0900

    230131 recommit

--- fulfill_rest/fulfill/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-
-def index(request):
-    # return HttpResponse('메인페이지')
-    return render(request,'index.html')
