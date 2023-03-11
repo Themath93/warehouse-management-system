d03e43821b7a1519751fcf23f4b54780846afecb
themath93 <lms4678@naver.com>
2023-03-11 15:16:03 +0900

    203011 xl warehouse delploy 1.137

--- xlwings_job/xl_test.py
+++ xlwings_job/xl_test.py
@@ -2,10 +2,6 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-# os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-# 위 명령어는 아래 의 경로를 말한다.
-# c:\users\"username"\desktop\fulfill
-
 
 
 import xlwings as xw
